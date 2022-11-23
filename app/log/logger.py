7a6a1e9fe5c0b9d42cb899e2d66658b3e49bbc74
NoobforAl <58856931+NoobforAl@users.noreply.github.com>
2022-11-23 16:33:44 +0330

    add log file & init file
    Signed-off-by: NoobforAl <58856931+NoobforAl@users.noreply.github.com>

---
+++ app/log/logger.py
@@ -0,0 +1,38 @@
+from logging.config import dictConfig
+from pydantic import BaseModel
+
+import logging
+
+
+class LogConfig(BaseModel):
+    """Logging configuration to be set for the server"""
+
+    LOGGER_NAME: str = "Syatem Log"
+    LOG_FORMAT: str = "%(levelprefix)s | %(asctime)s | %(message)s"
+    LOG_LEVEL: str = "DEBUG"
+
+    # Logging config
+    version = 1
+    disable_existing_loggers = False
+    formatters = {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": LOG_FORMAT,
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    }
+    handlers = {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+    }
+    loggers = {
+        "SyatemLog": {"handlers": ["default"], "level": LOG_LEVEL},
+    }
+    filename = 'app/log/logs/file.log'
+
+
+dictConfig(LogConfig().dict())
+log = logging.getLogger("SyatemLog")
