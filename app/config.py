28ed074eb8457c1f38e087fc9ca9a52935e07655
NoobforAl <58856931+NoobforAl@users.noreply.github.com>
2022-11-29 23:46:20 +0330

    add url mongodb
    Signed-off-by: NoobforAl <58856931+NoobforAl@users.noreply.github.com>

--- app/config.py
+++ app/config.py
@@ -6,11 +6,12 @@ import os
 load_dotenv(dotenv_path=Path('../.env'))
 
 
-SQLALCHEMY_DATABASE_URL: str = os.getenv(
-    "DATABASE_URL") or "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL: str = os.getenv("DATABASE_URL") or \
+    "sqlite:///./sql_app.db"
 
 REDIS_HOST: str = os.getenv("REDIS_URL") or "localhost"
 REDIS_PORT: int = int(os.getenv("REDIS_PORT") or 6379)
 REDIS_PASS: str = os.getenv("REDIS_PASS") or ""
 
-MONGODB_URL: str = os.getenv("MONGODB_URL") or ""
+MONGODB_URL: str = os.getenv("MONGODB_URL") or \
+    "mongodb://mongodb:mongodb@mongodb:27017/testDB"
